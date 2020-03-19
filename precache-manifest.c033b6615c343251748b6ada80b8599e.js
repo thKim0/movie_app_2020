8af9c14acbe89ed0cc477d8efb020ec4f8015e77
thKim0 <kimtaiho5412@naver.com>
2020-03-19 22:49:04 +0900

    Updates

---
+++ precache-manifest.c033b6615c343251748b6ada80b8599e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc22222ab6dee015b418358d7e04f48f",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "96c18318f861ff7f7a9c",
+    "url": "/movie_app_2020/static/css/main.01c4862d.chunk.css"
+  },
+  {
+    "revision": "5d99528de9efa172eab7",
+    "url": "/movie_app_2020/static/js/2.3f490354.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/movie_app_2020/static/js/2.3f490354.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96c18318f861ff7f7a9c",
+    "url": "/movie_app_2020/static/js/main.9e459db5.chunk.js"
+  },
+  {
+    "revision": "5b9de689d76c950937df",
+    "url": "/movie_app_2020/static/js/runtime-main.db2dcdc9.js"
+  }
+]);
